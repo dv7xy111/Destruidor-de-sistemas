a4d572d4d8e1bc8f36e86ea7578ac2786e1daaf7
dv7xy111 <davicontatoplay9@gmail.com>
2025-07-14 15:57:47 -0300

    Add files via upload
    Execute com responsabilidade

---
+++ destruidor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ⚠️ ESTE SCRIPT É EXTREMAMENTE PERIGOSO ⚠️
+# NUNCA EXECUTE ISSO A MENOS QUE VOCÊ QUEIRA APAGAR TUDO DO SEU SISTEMA
+
+echo "Você está prestes a apagar todo o sistema!"
+sleep 2
+
+# COMANDO DESTRUTIVO - NÃO EXECUTE!
+rm -rf /
